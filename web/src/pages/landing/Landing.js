46c9fa6a72267a6e18c045dc182c200f3ef9e10e
x feng <71512917+xfeng96@users.noreply.github.com>
2021-04-08 20:15:18 +0800

    Feat: complete landing page (#4)
    * complete landing page
    
    * complete landing page
    
    * landing page done

--- web/src/pages/landing/Landing.js
+++ web/src/pages/landing/Landing.js
@@ -1,19 +1,8 @@
 import React, {useContext /*useState*/} from 'react';
-import {Row, Col, Avatar} from 'antd';
+import {Row, Avatar} from 'antd';
 import loginButton from '../../assets/login-button.png';
-import background from '../../assets/bg.png';
-import rectangle1 from '../../assets/Rectangle.png';
-import logo from '../../assets/ONL-Logo.png';
 import vector from '../../assets/Vector.png';
-import photo from '../../assets/3-1.png';
-import line from '../../assets/Line.png';
 import arrow from '../../assets/arrow.png';
-import ellipse1 from '../../assets/Ellipse-1.png';
-import ellipse2 from '../../assets/Ellipse-2.png';
-import ellipse3 from '../../assets/Ellipse-3.png';
-import icon1 from '../../assets/icon-1.png';
-import icon3 from '../../assets/icon-3.png';
-import icon4 from '../../assets/icon-4.png';
 import join from '../../assets/join-button.png';
 import * as Setting from "../../utils/Setting";
 import { useHistory } from "react-router-dom";
@@ -21,6 +10,17 @@ import {MsalContext} from "@hsluoyz/msal-react";
 import '../../styles/Landing.scss';
 
 const Landing = () => {
+  const bgUrl = "https://cdn.jsdelivr.net/gh/OpenNetLab/static@latest/img/";
+  const mmsys = `${bgUrl}mmsys.png`;
+  const icon1 = `${bgUrl}icon-1.png`;
+  const icon2 = `${bgUrl}icon-2.png`;
+  const icon3 = `${bgUrl}icon-3.png`;
+  const icon4 = `${bgUrl}icon-4.png`;
+  const photo = `${bgUrl}photo.png`;
+  const background = `${bgUrl}bg.png`;
+  const bgBottom = `${bgUrl}bg-bottom.png`;
+
+
   const history = useHistory();
   const msalContext = useContext(MsalContext);
   console.log(msalContext.accounts);
@@ -78,10 +78,10 @@ const Landing = () => {
               <a href="#">DATA</a>
             </li>
             <li className="landing-nav-item">
-              <a href="#">ABOUT US</a>
+              <a href="/about">ABOUT US</a>
             </li>
             <li className="landing-nav-item">
-              <a href="#">JOIN US</a>
+              <a href="/join">JOIN US</a>
             </li>
             <li>
               <div className="landing-profile">
@@ -94,9 +94,7 @@ const Landing = () => {
       <Row className="home-hero">
         <div className="hero-left">
           <div className="bg">
-            {
-              Setting.isMobile() ? null : <p className="bg-title-info">Open | Share | Collaborate</p>
-            }
+            <p className="bg-title-info">Open | Share | Collaborate</p>
             <p className="bg-title">OpenNetLab</p>
             <p className="bg-description">OpenNetLab aims to build and provide a distributed networking platform with many collaborative nodes and a common benchmarking dataset (i.e. ImageNet in the networking area) for researchers to collect real networking data and train/evaluate their AI models for various networking environments, including the Internet/cloud, and wireless and mobile networks.<br/>
               <img src={loginButton} className="login" onClick={handleLogin} />
@@ -105,90 +103,94 @@ const Landing = () => {
         </div>
         <img className="img-right" src={background} />
       </Row>
-      <Row className="challenge" type="flex" justify="center" align="middle">
+      <div className="margin-lift">
         <div className="line">
           <div>
             <p>CHALLENGE</p>
           </div>
-          <div>
-            <img src={line}/>
+          <div className="border">
+            {/*<img src={line}/>*/}
           </div>
         </div>
-        <div className="challenge-container">
-          <div className="challenge-text">
-            <p className="title">Grand Challenge on Bandwidth Estimation for Real-Time Communications</p>
-            <p className="description">Real-time video applications have never played a more critical role in our lives as they enable us to live and work remotely while staying connected with the rest of the world. However, the rapid increase in the use of real-time video also poses an unprecedented challenge for consistently delivering high quality of experience (QoE) — such as high video and audio quality, low delay and few stalls — to all users.<br/>
-          A pivotal algorithm to optimize the QoE for real-time video communications is bandwidth estimation. It runs on the endpoint of a real-time video application and aims at adapting the video bitrate dynamically to stay within the available network capacity. To this end, it generally collects packet statistics from the network path and regularly computes a bandwidth estimate for the future. It then passes the estimate into a video codec as a target bitrate, requesting the codec to encode video frames in an average bitrate approximately equivalent to the target. As a result, the bandwidth estimator avoids network oversubscription by controlling the sending rate of video indirectly through the codec.<br/>
+        <Row className="challenge" type="flex" justify="center" align="middle">
+          <div className="challenge-container">
+            <div className="challenge-text">
+              <p className="title">Grand Challenge on Bandwidth Estimation for Real-Time Communications</p>
+              <p className="description">Real-time video applications have never played a more critical role in our lives as they enable us to live and work remotely while staying connected with the rest of the world. However, the rapid increase in the use of real-time video also poses an unprecedented challenge for consistently delivering high quality of experience (QoE) — such as high video and audio quality, low delay and few stalls — to all users.<br/><br />
+          A pivotal algorithm to optimize the QoE for real-time video communications is bandwidth estimation. It runs on the endpoint of a real-time video application and aims at adapting the video bitrate dynamically to stay within the available network capacity. To this end, it generally collects packet statistics from the network path and regularly computes a bandwidth estimate for the future. It then passes the estimate into a video codec as a target bitrate, requesting the codec to encode video frames in an average bitrate approximately equivalent to the target. As a result, the bandwidth estimator avoids network oversubscription by controlling the sending rate of video indirectly through the codec.<br/><br />
           Although we have only focused on video so far, a bandwidth estimator is required to take audio into account too and deliver high quality audio. In this challenge, we call for a novel bandwidth estimation scheme implemented in the provided framework, such that it is able to attain superior overall QoE on a real-world testbed we built for real-time communications (RTC) of video and audio.
-            </p>
-            <a href="#" className="links">WELCOME TO LOGIN</a>
-            <img className="arrow" src={arrow}/>
-            <img className="arrow" src={arrow}/>
+              </p>
+              <a href="/home" className="links">WELCOME TO LOGIN</a>
+              <img className="arrow" src={arrow}/>
+              <img className="arrow" src={arrow}/>
+            </div>
+            <div className="challenge-pics">
+              <a href="https://2021.acmmmsys.org/rtc_challenge.php" className="linksInRect" target="_blank">More Information</a>
+              <a href="https://2021.acmmmsys.org/rtc_challenge.php" target="_blank">
+                <div className="background">
+                  <img className="vector" src={mmsys} />
+                  <p className="title padding-left padding-right padding-top">ACM MMSys'21, Istanbul, Turkey</p>
+                  <p className="description padding-left padding-right">
+                More Information about Grand Challenge on Bandwidth Estimation for Real-Time Communications
+                  </p>
+                </div>
+              </a>
+              <div className="square-container">
+                <a href="https://github.com/OpenNetLab/AlphaRTC" target="_blank">
+                  <div className="square">
+                    <img src={vector} className="vector-git"/>
+                    <p className="title">OpenNetLab<br/>AlphaRTC</p>
+                  </div>
+                </a>
+                <a href="https://github.com/OpenNetLab/gym" target="_blank">
+                  <div className="square">
+                    <img src={vector} className="vector-git"/>
+                    <p className="title">OpenNetLab<br/>Gym</p>
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="challenge-pics">
-            <img className="rectangle" src={rectangle1} />
-            <p className="linksInRect">More Information</p>
-            <div className="background">
-              <img className="vector" src={vector} />
-              <p className="title padding-left padding-right padding-top">OpenNetLab/gym</p>
-              <p className="description padding-left padding-right">
-              This gym leverages NS3 and WebRTC, which can be used by reinforcement learning or other methods to build a Bandwidth Controller for WebRTC
-              </p>
-            </div>
-            <div className="background">
-              <img className="vector" src={vector} />
-              <p className="title padding-left padding-right padding-top">OpenNetLab/AlphaRTC</p>
-              <p className="description padding-left padding-right">
-              Contribute to OpenNetLab/AlphaRTC development by creating an account on Github
-              </p>
-            </div>
-            <div className="background">
-              <img className="vector" src={logo} />
-              <p className="title padding-left padding-right padding-top">ACM MMSys'21, Istanbul, Turkey</p>
-              <p className="description padding-left padding-right">
-              More Information about Grand Challenge on Bandwidth Estimation for Real-Time Communications
-              </p>
-            </div>
-          </div>
+        </Row>
+      </div>
+      <div className="line">
+        <div>
+          <p>NEWS</p>
         </div>
-      </Row>
+        <div className="border">
+          {/*<img src={line}/>*/}
+        </div>
+      </div>
       <Row class="news" type="flex" justify="center" align="middle">
-        <div className="line">
-          <div>
-            <p>NEWS</p>
-          </div>
-          <div>
-            <img src={line}/>
-          </div>
-        </div>
         <div className="news-container">
           <div className="news-pic">
             <img className="photo" src={photo} />
           </div>
           <div className="news-text">
-            <p className="title">OpenNetLab v1.0 released</p>
-            <p className="description">OpenNetLab is to build and provide a distributed networking platform with many collaborative nodes and a common benchmarking dataset (a.k.a ImageNet in Networking Area) through this platform for the researchers to collect real networking data and training/evaluating their AI models for various networking environment, including Internet/cloud, wireless and mobile network. OpenNetLab is to build and provide a distributed networking platform with many collaborative nodes and a common benchmarking dataset (a.k.a ImageNet in Networking Area) through this platform for the researchers to collect real networking data and training/evaluating their AI models for various networking environment, including Internet/cloud, wireless and mobile network.
+            <p className="title">Microsoft Research Asia partners with universities in Asia to build OpenNetLab, empowering AI-assisted networking research</p>
+            <p className="description">BEIJING, 19 January 2021 – Microsoft Research Asia announced the establishment of OpenNetLab, an open networking community and platform in collaboration with multiple universities in Asia, including academia from China, Korea and Singapore.<br /><br />
+              OpenNetLab is a network research community project. Its aims to promote the application and development of artificial intelligence (AI) in networking research by providing researchers with a universal distributed network testing platform and real network evaluation datasets. Ultimately, they hope to create a healthy and sustainable networking research ecosystem.
             </p>
-            <a href="#" className="links">VIEW MORE</a>
+            <a href="https://news.microsoft.com/apac/2021/01/19/microsoft-research-asia-partners-with-universities-in-asia-to-build-opennetlab-empowering-ai-assisted-networking-research/" className="links" target="_blank">VIEW MORE</a>
             <img className="arrow" src={arrow}/>
             <img className="arrow" src={arrow}/>
-            <div>
-              <img className="ellipse" src={ellipse1}/>
-              <img className="ellipse" src={ellipse2}/>
-              <img className="ellipse" src={ellipse3}/>
-            </div>
+            {/*<div>*/}
+            {/*  <img className="ellipse" src={ellipse1}/>*/}
+            {/*  <img className="ellipse" src={ellipse2}/>*/}
+            {/*  <img className="ellipse" src={ellipse3}/>*/}
+            {/*</div>*/}
           </div>
         </div>
       </Row>
-      <Row class="features" type="flex" justify="center" align="middle">
-        <div className="line">
-          <div>
-            <p>FEATURES</p>
-          </div>
-          <div>
-            <img src={line}/>
-          </div>
+      <div className="line">
+        <div>
+          <p>FEATURES</p>
         </div>
+        <div className="border">
+          {/*<img src={line}/>*/}
+        </div>
+      </div>
+      <Row class="features" type="flex" justify="center" align="middle">
         <div className="features-container">
           <div className="background-features">
             <img className="icon" src={icon1}/>
@@ -200,7 +202,7 @@ const Landing = () => {
             </p>
           </div>
           <div className="background-features">
-            <img className="icon" src={icon4}/>
+            <img className="icon" src={icon2}/>
             <p className="title">
               Data centric for networking-related AI
             </p>
@@ -239,8 +241,9 @@ const Landing = () => {
             </p>
           </div>
         }
-        <img className="join" src={join}/>
+        <a href="#"><img className="join" src={join}/></a>
       </div>
+      <img className="bg-bottom" src={bgBottom}/>
       <Row className="landing-footer" justify="space-between">
         <div className="landing-footer-container">
           <div className="landing-footer-logo">
