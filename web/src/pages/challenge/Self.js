046fbd8389f377404e55ce214a434806bda02b25
xfeng <fx96@stu.pku.edu.cn>
2021-05-27 00:53:46 +0800

    Fix table text display

--- web/src/pages/challenge/Self.js
+++ web/src/pages/challenge/Self.js
@@ -60,25 +60,25 @@ const Self = () => {
       render: status => (
         <>
           {
-            status == "0" ? <><div className="run"></div>&nbsp;&nbsp;<b>Init</b></> : null
+            status == "0" ? <div className="status"><div className="run"></div>&nbsp;&nbsp;<b>Init</b></div> : null
           }
           {
-            status == "1" ? <><div className="run"></div>&nbsp;&nbsp;<b>Created</b></> : null
+            status == "1" ? <div className="status"><div className="run"></div>&nbsp;&nbsp;<b>Created</b></div> : null
           }
           {
-            status == "2" ? <><div className="run"></div>&nbsp;&nbsp;<b>Running</b></> : null
+            status == "2" ? <div className="status"><div className="run"></div>&nbsp;&nbsp;<b>Running</b></div> : null
           }
           {
-            status == "3" ? <><div className="suc"></div>&nbsp;&nbsp;<b>Succeeded</b></> : null
+            status == "3" ? <div className="status"><div className="suc"></div>&nbsp;&nbsp;<b>Succeeded</b></div> : null
           }
           {
-            status == "4" ? <><div className="fail"></div>&nbsp;&nbsp;<b style={{color:'#8e8e8e'}}>Failed</b></> : null
+            status == "4" ? <div className="status"><div className="fail"></div>&nbsp;&nbsp;<b style={{color:'#8e8e8e'}}>Failed</b></div> : null
           }
           {
-            status == "5" ? <><div className="run"></div>&nbsp;&nbsp;<b>DeferSelection</b></> : null
+            status == "5" ? <div className="status"><div className="run"></div>&nbsp;&nbsp;<b>DeferSelection</b></div> : null
           }
           {
-            status == null ? <><div className="blank"></div>&nbsp;&nbsp;<b style={{color:'#8e8e8e'}}>Unknown</b></> : null
+            status == null ? <div className="status"><div className="blank"></div>&nbsp;&nbsp;<b style={{color:'#8e8e8e'}}>Unknown</b></div> : null
           }
         </>
       ),
